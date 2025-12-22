b988a823c9d80052cabae1c3eae079ef191a576f
Anish GM <138877248+anishgm07@users.noreply.github.com>
2025-12-22 16:09:00 +0530

    Updated Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -10,8 +10,8 @@ export default function Header() {
     const [isDonationOpen, setIsDonationOpen] = useState(false);
 
     return (
-        <header className="bg-[#222222CC] text-[#DEDEDE] py-3 md:py-3 top-4 z-50 shadow sticky max-w-6xl mx-4 md:mx-auto mt-3 rounded-lg">
-            <div className="max-w-6xl mx-auto flex items-center justify-between px-3 font-poppins">
+        <header className="bg-[#222222CC] text-[#DEDEDE] py-3 md:py-3 top-4 z-50 shadow sticky mx-4 md:mx-auto mt-3 rounded-lg">
+            <div className="flex items-center justify-between px-3 font-poppins">
                 {/* Logo */}
                 <div className="flex items-center flex-shrink-0 bg-[#3E3E3E] rounded-[10px] p-[0px] md:p-[6px]">
                     <Link href="/home">
